66c978f3675a23395164bddc9cc14c237845bbd7
aberlanga-gatech <70611360+aberlanga-gatech@users.noreply.github.com>
2021-11-17 23:15:16 -0500

    commit

--- blackjack.py
+++ blackjack.py
@@ -5,6 +5,7 @@ RANKS = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 VALUES = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 BUY_IN = 100
 
+# game state
 playing = True
 
 # card class
